9fd7146fc80c36063847a09e24689711aec6a923
spenclark <spenclark@hotmail.com>
2019-12-04 10:45:23 -0800

    server.js built

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const postRouter = require('../posts/router');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('<h2> POST API <H2>')
+})
+
+server.use('/api/posts', postRouter);
+
+module.exports = server;
